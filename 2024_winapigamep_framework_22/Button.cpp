7d372c39fff08eb899d625974d9fbbb427c496f5
Chamom1ie <GitHubmile@gmail.com>
2024-12-02 10:42:54 +0900

    구조변경 20회차

---
+++ 2024_winapigamep_framework_22/Button.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "Button.h"
+
+Button::Button()
+{
+}
+
+Button::Button(Vec2 size, Vec2 pos)
+{
+	m_size = size;
+	m_pos = pos;
+}
+
+Button::~Button()
+{
+}
--- 2024_winapigamep_framework_22/InventoryManager.h
+++ 2024_winapigamep_framework_22/InventoryManager.h
@@ -1,6 +1,7 @@
 #pragma once
 class Cell;
 class Battery;
+class Button;
 class InventoryManager
 {
 	DECLARE_SINGLE(InventoryManager);
@@ -9,7 +10,8 @@ private:
 private:
 	void Show();
 	void Hide();
-	void TryInteract(Vec2 _mousePos);
+	void TryInteractToSeri(Vec2 _mousePos);
+	void TryInteractToPara(Vec2 _mousePos);
 public:
 	void Init();
 	void Update();
@@ -18,9 +20,12 @@ public:
 private:
 	bool m_activeSelf = false;
 	int m_InventorySize = SCREEN_WIDTH;
-	vector<Battery*> m_vecBatteries;
-	Vec2 m_curCell {};
-	Battery* m_curBattery = nullptr;
-	Battery* m_batteryCells[6][6] = {};
+	int m_seriCount = 0;
+	int m_paraCount = 0;
+	int m_batteryCount = 0;
+	Button* m_addSeriBtn;
+	Button* m_subSeriBtn;
+	Button* m_addParaBtn;
+	Button* m_subParaBtn;
 };
 
