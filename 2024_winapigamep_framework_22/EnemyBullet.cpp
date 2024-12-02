8ee280164f9455e9b20b0c26b4d924bf41c77869
luca3 <dudsj9782@gmail.com>
2024-12-02 14:58:01 +0900

    enemyBullet

--- 2024_winapigamep_framework_22/EnemyBullet.cpp
+++ 2024_winapigamep_framework_22/EnemyBullet.cpp
@@ -1,2 +1,34 @@
 #include "pch.h"
+#include "EventManager.h"
 #include "EnemyBullet.h"
+#include "Collider.h"
+
+EnemyBullet::EnemyBullet()
+{
+}
+
+EnemyBullet::~EnemyBullet()
+{
+}
+
+void EnemyBullet::Render(HDC _hdc)
+{
+	ELLIPSE_RENDER(_hdc, m_vPos.x, m_vPos.y, m_vSize.x, m_vSize.y);
+}
+
+void EnemyBullet::EnterCollision(Collider* _other)
+{
+	Object* pOtherObj = _other->GetOwner();
+	if (pOtherObj->GetName() == L"Player")
+	{
+		GET_SINGLE(EventManager)->DeleteObject(this);
+	}
+}
+
+void EnemyBullet::StayCollision(Collider* _other)
+{
+}
+
+void EnemyBullet::ExitCollision(Collider* _other)
+{
+}
--- 2024_winapigamep_framework_22/EnemyBullet.h
+++ 2024_winapigamep_framework_22/EnemyBullet.h
@@ -2,4 +2,12 @@
 #include "Projectile.h"
 class EnemyBullet : public Projectile
 {
+public:
+	EnemyBullet();
+	~EnemyBullet();
+	void Render(HDC _hdc) override;
+public:
+	void EnterCollision(Collider* _other) override;
+	void StayCollision(Collider* _other) override;
+	void ExitCollision(Collider* _other) override;
 };
